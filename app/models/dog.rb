66b2ddbf5f24ad79bea8e8fe1f4ddfd4df6c2f71
victorious-messaging-4309 <wildayk@gmail.com>
2019-04-22 18:55:48 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,12 +2,12 @@ class Dog
   attr_accessor :name, :breed, :age
 
   DOGS = []
-  
+
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
-    DOGS << self 
+    DOGS << self
   end
 
   def self.all
